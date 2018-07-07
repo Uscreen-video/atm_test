026f20ad1b1d6cf01c0885360fb7f15bcfbed6d5
Anastasia <anastasiamaschenko103@gmail.com>
2018-07-07 21:08:24 +0300

    witdraw money

--- app.rb
+++ app.rb
@@ -4,3 +4,23 @@ get '/' do
   erb :index
 end
 
+post '/' do
+  json witdraw(params[:amount].to_i)
+end
+
+def witdraw(amount)
+  result = []
+  while amount / 50 > 0
+    result << 50
+    amount -= 50
+  end
+  while amount / 20 > 0
+    result << 20
+    amount -= 20
+  end
+  while amount / 5 > 0
+    result << 5
+    amount -= 5
+  end
+  result
+end
